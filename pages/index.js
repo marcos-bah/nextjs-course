c03bb0eadc3de912a5bcf366e404518f4b65c136
marcos-bah <marcos.barbosa@unfei.edu.br>
2022-02-03 09:49:02 -0300

    add arquivos iniciais

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import Link from '../src/components/Link';
+// import Footer from '../src/components/patterns/Footer';
+import HomeScreen from '../src/screens/HomeScreen';
+
+export default HomeScreen;
+// function Title({children, as}) {
+//   const Tag = as;
+//   return (
+//     <React.Fragment>
+//       <Tag>{children}</Tag>
+//       <style jsx>{
+//         `${Tag} {
+//           font-size: 1.5em;
+//           text-align: center;
+//           color: #333;
+//         }`
+//       }</style>
+//     </React.Fragment>
+    
+//   );
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h1">Home Page </Title>
+//       <Link href='/faq'>Ir para a página faq</Link>
+//       <Footer/>
+//     </div>
+//   );
+// }
